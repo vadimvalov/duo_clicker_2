d0f74dd75a1d207db4c9a8a3b1f9051463c67f83
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-03-03 00:27:22 +0600

    feat: screen endpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens :{
+      'lg': {'max': '992px'},
+      'md': {'max': '768px'},
+      'sm': {'max': '480px'}
+    },
+
     extend: {},
   },
   plugins: [],
